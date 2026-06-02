9442efa2c219cfa7ce9159cfd6463fc60e09748d
nabdcm <raidabw31@gmail.com>
2026-06-02 22:34:42 +0300

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -369,6 +369,7 @@ const T = {
       successMsg: "Pharmacy created successfully!",
     },
     subModal: {
+      title:"Edit Subscription",
       tabInfo:"Info",
       tabSub:"Subscription",
       tabSecurity:"Security",
